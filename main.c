acba4e0d4604ea5d290d9ba111bb022e20f62ae0
Elias Benmellouk <ebenme01@vm-hw08.eecs.tufts.edu>
2022-04-16 19:24:18 -0400

    Basically finished the game, only thing remaining to do is make a real random generator function bc the one we have is only pseudo-random

--- main.c
+++ main.c
@@ -1,36 +1,50 @@
 #include "stm32l476xx.h"
 #include "LCD.h"
 #include "SysTick.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-void display_time(int time_to_display);
+int display_milliseconds(int time_to_display);
 void display_direction(int index);
+void display_score(int score);
 void joystick_config(void);
-void joystick_test(void);
-
+void count_down(void);
 void movingString(uint8_t* str, uint8_t len);
 void start_screen(void);
+int random_direction_gen(void);
+int count_until_click(int index);
 
 int main(void){
 	int index = 0;
+	int time_elapsed = 0;
+	int dir_index = 0;
+	int millis_elapsed = 0;
+	int score_increment = 0;
+	int score = 0;
 	
 	LCD_Initialization(); //initialize the LCD display
 	SysTick_Initialize(1000);
 	joystick_config();
-	//joystick_test();
-	
 	
 	start_screen();
-
-	display_direction(2);
+	while(1){
+		count_down();
+		dir_index = random_direction_gen();
+		display_direction(dir_index);
+		millis_elapsed = count_until_click(dir_index);
+		score_increment = display_milliseconds(millis_elapsed);
+		delay(1000);
+		score = score + score_increment;
+		display_score(score);
+		delay(1000);
+	}
 }
-
 
 
 //Displays a direction: up, down, right, left, or pushdown
 //Each direction has a corresponding index input
 //Input: index of the direction to display to the LCD
 //Output: Direction string on the LCD
-//TESTED: WORKS
 void display_direction(int index){
 	
 	char *up = "  UP  ";    //0
@@ -40,72 +54,29 @@ void display_direction(int index){
 	char *push = " PUSH ";	//4
 	char *error = "ERROR!"; //if wrong input, output error
 	
-	if(index == 0){
-		LCD_DisplayString(up);
-	}else if(index == 1){
-		LCD_DisplayString(down);
-	}else if(index == 2){
-		LCD_DisplayString(right);
-	}else if(index == 3){
-		LCD_DisplayString(left);
-	}else if(index == 4){
-		LCD_DisplayString(push);
-	}else{
-		LCD_DisplayString(error);
+	switch (index) {
+		case 0:
+			LCD_DisplayString(up);
+			break;
+		case 1:
+			LCD_DisplayString(down);
+			break;
+		case 2:
+			LCD_DisplayString(right);
+			break;
+		case 3:
+			LCD_DisplayString(left);
+			break;
+		case 4:
+			LCD_DisplayString(push);
+			break;
+		default:
+			LCD_DisplayString(error);
 	}
-	
-}
-
-//Displays an amount of milliseconds in mm:ss,ss format
-//input: time in milliseconds to display
-//output: display to LCD
-//TESTED: WORKS
-void display_time(int elapsed){
-	
-	//variable declarations
-	char minutes_1_char[1];
-	char minutes_2_char[1];
-	char seconds_ints_1_char[1];   //integer seconds
-	char seconds_ints_2_char[1];	 //decimal seconds
-	char seconds_decs_1_char[1];
-	char seconds_decs_2_char[1];
-	int minutes;
-	int seconds_ints;
-	int seconds_decs;
-	int minutes_1;
-	int minutes_2;
-	int seconds_ints_1;
-	int seconds_ints_2;
-	int seconds_decs_1;
-	int seconds_decs_2;
-	
-	
-	minutes_1 = ((elapsed - (elapsed % 60000))/60000)/10;              //first 2 characters
-	minutes_2 = ((elapsed - (elapsed % 60000))/60000)%10;
-	seconds_ints_1 = (((elapsed % 60000) - (elapsed % 1000))/1000)/10; //middle 2 characters
-	seconds_ints_2 = (((elapsed % 60000) - (elapsed % 1000))/1000)%10;
-	seconds_decs_1 = ((elapsed % 1000)/10)/10;                         //last 2 characters
-	seconds_decs_2 = ((elapsed % 1000)/10)%10; 
-	
-	//convert to characters
-	*minutes_1_char = minutes_1 + '0';				
-	*minutes_2_char = minutes_2 + '0';
-	*seconds_ints_1_char = seconds_ints_1 + '0';
-	*seconds_ints_2_char = seconds_ints_2 + '0';
-	*seconds_decs_1_char = seconds_decs_1 + '0';
-	*seconds_decs_2_char = seconds_decs_2 + '0';
-	
-	//Output to LCD
-	LCD_WriteChar(minutes_1_char, 0 , 0, 0); 
-	LCD_WriteChar(minutes_2_char, 0 , 1, 1);  
-	LCD_WriteChar(seconds_ints_1_char, 0 , 0, 2);  
-	LCD_WriteChar(seconds_ints_2_char, 1 , 0, 3); 
-	LCD_WriteChar(seconds_decs_1_char, 0 , 0, 4); 
-	LCD_WriteChar(seconds_decs_2_char, 0 , 0, 5);
-	
 }
 
 
+//Configure all joystick buttons. clock, GPIO mode, etc
 void joystick_config(void){
 	//Enable clock for GPIO port A
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
@@ -126,68 +97,8 @@ void joystick_config(void){
 	GPIOA->MODER &= ~(0x00000C00);
 }
 
-//Test the joystick functioning
-//Each time the user presses down on the button, a counter increments 
-//depending on direction, different increment
-//TESTED: WORKS
-void joystick_test(void){
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int d = 0;
-	int e = 0;
-	int a_prev = 0;
-	int b_prev = 0;
-	int c_prev = 0;
-	int d_prev = 0;
-	int e_prev = 0;
-	
-	int count = 0;
-	
-	while(1){
-		a = ((GPIOA->IDR) & 0x00000001); //0th bit
-		b = ((GPIOA->IDR) & 0x00000002) >> 1; //1nd bit
-		c = ((GPIOA->IDR) & 0x00000004) >> 2; //2rd bit
-		d = ((GPIOA->IDR) & 0x00000008) >> 3; //3rd bit
-		e = ((GPIOA->IDR) & 0x00000020) >> 5; //4rd bit
-		
-		if((a == 1)&&(a_prev==0)){
-			count = count + 60000;
-		}
-		
-		if((b == 1)&&(b_prev==0)){
-			count = count + 10000;
-		}
-		
-		if((c == 1)&&(c_prev==0)){
-			count = count + 1000;
-		}
-		
-		if((d == 1)&&(d_prev==0)){
-			count = count + 100;
-		}
-		
-		if((e == 1)&&(e_prev==0)){
-			count = count + 10;
-		}
-		
-		display_time(count);
-		
-		
-		
-		
-		
-		a_prev = a;
-		b_prev = b;
-		c_prev = c;
-		d_prev = d;
-		e_prev = e;
-		
-	}
-}
 
-
-
+//Scrolls the starting message accross the LCD. Stops when the central button is pressed.
 void movingString(uint8_t* str, uint8_t len){
 	int a = 0;
 	int position = 0;
@@ -203,10 +114,8 @@ void movingString(uint8_t* str, uint8_t len){
 	
 	str_pad = (char *) malloc(length); //allocate more space
 	
-	
 	while(1){
 		//copy the contents of str into str_pad. 
-		
 		for(i = 0; i <= len; i++){
 			str_pad[i] = str[i];
 		}
@@ -216,14 +125,11 @@ void movingString(uint8_t* str, uint8_t len){
 			str_pad[i] = ' ';
 			i = i + 1;
 		}
-		
-		
 		//shift the ensemble of elements display to the right to give the illusion of scrolling to the left. 
 		for(i = 0; i <= length; i++){
 			LCD_DisplayString(str_pad+i);		
-			
 			for(j = 0; j < 100000; j++){
-		    if(((GPIOA->IDR) & 0x00000001) == 1) return;
+		    if(((GPIOA->IDR) & 0x00000001) == 1) return;  //start game
 	    }
 		}
 		//shift back in opposite direction
@@ -240,24 +146,164 @@ void movingString(uint8_t* str, uint8_t len){
 			LCD_DisplayString(str_pad+i);			
 			
 			for(j = 0; j < 100000; j++){
-		    if(((GPIOA->IDR) & 0x00000001) == 1) return;
+		    if(((GPIOA->IDR) & 0x00000001) == 1) return;  //start game
 	    }
 		}
+	}
+}
+
+//Display the game starting message. Game starts when the central button is pressed
+void start_screen(void){
+	char *start_message = "PRESS BUTTON TO START THE GAME";
+	movingString((uint8_t *)start_message, strlen(start_message));
+}
+
+
+//Returns time in milliseconds between center press and given side press
+//input: index of the side button we want to count for.
+//output: Time (in milliseconds) elapsed
+int count_until_click(int index){
+	int count = 0; //counter
+	int time_divider = 305;  //adjust the time by dividing
+	int time_elapsed = 0;
+	int button_status = 0;
+	
+	int missed_exit_code = 777; //if the player exceeds 500 ms, output 777.
+															//The printing function will recognize this 
+															//code and will output a "losing" message
+	if(index == 0){
+		button_status = ((GPIOA->IDR) & 0x00000008) >> 3; //up PA3
+	}else if(index == 1){
+		button_status = ((GPIOA->IDR) & 0x00000020) >> 5; //down PA5
+	}else if(index == 2){
+		button_status = ((GPIOA->IDR) & 0x00000004) >> 2; //right PA2
+	}else if(index == 3){
+		button_status = ((GPIOA->IDR) & 0x00000002) >> 1; //left PA1
+	}
+	
+	while (button_status == 0){
+		count = count + 1;
 		
+		//update status ints
+		if(index == 0){
+			button_status = ((GPIOA->IDR) & 0x00000008) >> 3; //up PA3
+		}else if(index == 1){
+			button_status = ((GPIOA->IDR) & 0x00000020) >> 5; //down PA5
+		}else if(index == 2){
+			button_status = ((GPIOA->IDR) & 0x00000004) >> 2; //right PA2
+		}else if(index == 3){
+			button_status = ((GPIOA->IDR) & 0x00000002) >> 1; //left PA1
+		}
 		
+		time_elapsed = count/time_divider;
+		
+		//if the player exceeds 500 ms, output 777. The printing function will 
+		//recognize this code and will output a "losing" message
+		if(time_elapsed > 600){
+			return missed_exit_code;
+		}
+	}
+	return time_elapsed;
+}
+
+
+//Displays time inputted with a "ms" appended
+//assumption that the input is 3 digits long (or less)
+int display_milliseconds(int time_to_display){
+	//variable declarations
+	char units[1];
+	char tens[1];
+	char hundreds[1];  
+	int units_int = 0;
+	int tens_int = 0;
+	int hundreds_int = 0;
+	char m[1];
+	char s[1];
+	char space[1];
+	
+	//if the player exceeds 500 ms, output a "losing" message
+	int missed_exit_code = 777;
+	char *missed_message = "MISSED";
+	if(time_to_display == missed_exit_code){
+		LCD_DisplayString(missed_message);
+		return 0;
+	}
+	
+	*m = 'm';  
+	*s = 's';
+	*space = ' ';
+	
+	//get each digit individually
+	hundreds_int = time_to_display/100;
+	tens_int = (time_to_display/10)%10;
+	units_int = time_to_display % 10;
+	//convert to character
+	*hundreds = hundreds_int + '0';
+	*tens = tens_int + '0';
+	*units = units_int + '0';
+	//print to LCD
+	LCD_WriteChar(hundreds, 0 , 0, 0); 
+	LCD_WriteChar(tens, 0 , 0, 1);  
+	LCD_WriteChar(units, 0 , 0, 2);
+	LCD_WriteChar(space, 0 , 0, 3);
+	LCD_WriteChar(m, 0 , 0, 4);  
+	LCD_WriteChar(s, 0 , 0, 5);
+	return 1;
+}
+
+
+//Display inputted number with "pts" appended
+//assumption that the score is less than 99
+void display_score(int score){
+	//variable declarations
+	char units[1];
+	char tens[1];  
+	int units_int = 0;
+	int tens_int = 0;
+
+	char p[1];
+	char t[1];
+	char s[1];
+	char space[1];
+	
+	tens_int = score/10;
+	units_int = score%10;
+	*tens = tens_int + '0';
+	*units = units_int + '0';
+	*p = 'p';  
+	*t = 't';
+	*s = 's';
+	*space = ' ';
+	
+	LCD_WriteChar(tens, 0 , 0, 0);  
+	LCD_WriteChar(units, 0 , 0, 1);
+	LCD_WriteChar(space, 0 , 0, 2);
+	LCD_WriteChar(p, 0 , 0, 3);  
+	LCD_WriteChar(t, 0 , 0, 4);
+	LCD_WriteChar(s, 0 , 0, 5);
+}
+
+
+//Starting countdown that is displayed before the direction is displayed
+void count_down(void){
+	int i;
+	char num[4];
+	num[0] = ' ';
+	num[1] = ' ';
+	num[2] = ' ';
+	for(i = 3; i > 0; i--){
+		num[3] = '0' + i;
+		LCD_DisplayString(num);
+		delay(500);
 	}
 }
 
 
-
-
-void start_screen(void){
-	
-	char *start_message = "PRESS BUTTON TO START THE GAME";
-	movingString((uint8_t *)start_message, strlen(start_message));
-	
+//Randomly generated a direction index
+//PROBLEM: IT'S PSEUDORANDOM. EACH TIME RESTART GAME, SAME SUCCESSION OF DIRECTIONS.
+int random_direction_gen(void){
+	int rand_num = rand(); // gen random number
+	rand_num %= 10; // take the ones place, this gives us a random number in the range [0, 10)
+	rand_num /= 2.5; // divide by 2.5, this shortens the range to [0,4).
+	return rand_num; // return an int, this floors the range, giving use the desired [0,3]
 }
-
-
-
-
